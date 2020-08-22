b1f60750b172ead58ab80479d39c705714bba1cc
mordurx <guido.mellado@gmail.com>
2020-08-22 09:54:07 -0400

    t

---
+++ coef_dif_paper.py
@@ -0,0 +1,43 @@
+from Dfit import *
+from decimal import Decimal
+# optional
+tmin = 10 # Minimum timestep used for MSD calculation
+tmax = 100 # Maximum timestep used for MSD calculation
+m = 20 # Number of MSD values in fitting
+nseg = 100 # Number of segments
+dt = 0.5 # dt*tunit is the timestep of the input trajectory
+path="/media/eniac/mdd1/paper_membranas/analisis/coef_diff_agua/"
+#path="/media/eniac/mdd1/paper_membranas/snx_water_coef_difu/dcd/"
+filename='agua_agua75nsindex 5799 5800 5801.dat'
+fz=path+filename
+
+fout=path+'coef_diff_paper'
+res_3D = Dfit.Dcov(fz=fz,tmin=tmin,tmax=tmax,m=m,nseg=nseg,dt=dt)
+res_3D.run_Dfit()
+
+
+
+
+# For analysis:
+tc = 5 # tc is connected to the timestep [in ps] by tc*dt. If tmin=1 and dt=1, then tc=7 corresponds to a 7ps timestep.
+
+# If desired, for finite-size correction (Yeh, Hummer, JPCB 2004):
+eta = 9.0e-4 # Viscosity in Pa*s
+L = 7.7 # Box edge length in nm (cubic box!)
+T = 300 # Temperature in Kelvin
+
+res_3D.analysis(tc=tc)
+res_3D.finite_size_correction(T=T,eta=eta,L=L,tc=tc)
+
+#0.004592890975343372 nm^2/ps
+def nm_ps_to_cm2_s(value= 0.004855):
+    value=float( '%.2E' % Decimal(value))
+    print (value)
+    ps_to_seg=1e-12
+    nm2_cm2=1e-14
+    cm2=value*nm2_cm2
+    print (cm2)
+    cm2_s=cm2/ps_to_seg
+    cm2_s='%.2E' % Decimal(cm2_s)
+    print (cm2_s)
+nm_ps_to_cm2_s()    
